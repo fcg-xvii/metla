66a4997308c249fd93ec6957e2fc77a9715dc89d
Vladimir V Maksimov <flint@77i.su>
2019-04-03 17:03:07 +0300

    Include completed...

---
+++ val_include.go
@@ -0,0 +1,65 @@
+package metla
+
+import _ "fmt"
+
+func init() {
+	keywords["include"] = keywordInclude
+}
+
+func keywordInclude(p *parser) (res interface{}, err error) {
+	res = &execCommand{p.infoRecordFromPos(), execInclude, "include"}
+	p.stack.Push(res)
+	if _, err = initCodeVal(p); err != nil {
+		return
+	}
+	p.PassSpaces()
+	if p.Char() != '\n' && p.Char() != '*' {
+		var obj interface{}
+		if obj, err = initCodeVal(p); err != nil {
+			return
+		}
+		if ex, mCheck := obj.(*execCommand); mCheck && ex.name != "init-object" {
+			err = p.positionError("Expected object variable")
+			return
+		}
+	} else {
+		p.stack.Push(nil)
+	}
+	return
+}
+
+func execInclude(exec *tplExec, info *rawInfoRecord) (err error) {
+	//fmt.Println("EXEC_INCLUDE")
+	nameObj, paramsObj, tplName := exec.st.Pop(), exec.st.Pop(), ""
+	if nameVar, check := nameObj.(*variable); check {
+		nameObj = nameVar.value
+	}
+	switch nameObj.(type) {
+	case string:
+		tplName = nameObj.(string)
+	default:
+		err = info.fatalError("template name string or variable expected")
+		return
+	}
+	if paramsObj != nil {
+		layout := exec.sto.newLayout()
+		m := paramsObj.(map[string]interface{})
+		for key, val := range m {
+			if v, check := val.(*variable); check {
+				val = v.value
+			}
+			layout.appendVariable(&variable{key, val, true})
+		}
+	}
+	var tpl *template
+	if tpl, err = exec.root.template(tplName); err == nil {
+		if err = tpl.checkUpdate(); err == nil {
+			err = tpl.result(exec.sto, exec.w)
+		}
+	}
+	if paramsObj != nil {
+		exec.sto.dropLayout()
+	}
+	//fmt.Println("INCLUDE_COMPLETED...")
+	return
+}

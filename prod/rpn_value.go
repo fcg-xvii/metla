25b3be1e913a3d0862f4857359e0cda2320af6bb
Vladimir V Maksimov <soer@77i.su>
2019-05-06 08:17:37 +0300

    rpn exec in work

---
+++ prod/rpn_value.go
@@ -0,0 +1,20 @@
+package prod
+
+import (
+	"reflect"
+)
+
+func rpnValueNumber(val interface{}, pos position, exec *tplExec) (float64, *execError) {
+	var rVal reflect.Value
+	switch val.(type) {
+	case getter:
+		rVal = reflect.ValueOf(val.(getter).get(exec))
+	case executer:
+		stackLen := exec.stack.Len()
+		if err := val.(executer).exec(tplExec); err == nil {
+
+		} else {
+			return 0, err
+		}
+	}
+}

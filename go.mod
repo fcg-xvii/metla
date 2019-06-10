edaf35550d99362d2bf3fc396faf297e3f3ec325
Vladimir V Maksimov <flint@77i.su>
2019-06-10 11:00:50 +0300

    update cache methods

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/fcg-xvii/metla
 go 1.12
 
 require (
-	github.com/fcg-xvii/containers v0.0.0-20190526143344-72b46da4895f
+	github.com/fcg-xvii/containers v0.0.0-20190608083024-7017c90fc381
 	github.com/fcg-xvii/lineman v0.0.0-20190314154620-36d40fa439af
 )
--- metla.go
+++ metla.go
@@ -33,11 +33,11 @@ func (s *Metla) SetMaxExecDuration(execTime time.Duration) {
 
 func (s *Metla) template(path string) (tpl *template, check bool) {
 	var iface interface{}
-	if iface, check = s.store.GetOrCreate(path, func(key interface{}, calls containers.CacheCallBacks) (interface{}, bool) {
+	if iface, check = s.store.GetOrCreate(path, nil, func(interface{}) (interface{}, interface{}, bool) {
 		if content, modified, exists, err := s.requester.RequestContent(path); exists && err == nil {
-			return newTemplate(s.requester, s, path, content, modified), true
+			return path, newTemplate(s.requester, s, path, content, modified), true
 		}
-		return nil, false
+		return nil, nil, false
 	}); check {
 		tpl = iface.(*template)
 	}
